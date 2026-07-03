99d40fef478cbffaf5927c96bcf60814615086a0
CaioPetrini <caio.asouza@sptech.school>
2026-07-03 15:24:32 -0300

    feat: add application entry point

---
+++ src/main.py
@@ -0,0 +1,15 @@
+def iniciarAplicacao():
+    mensagemDeBoasVindas = """
+        ====================================
+        MAIL AUTOMATION
+        ====================================
+
+        Bem-vindo ao Mail Automation!
+
+        Inicializando aplicação...
+        
+        Aplicação iniciada com sucesso!"""
+        
+    print(mensagemDeBoasVindas)
+          
+iniciarAplicacao()
